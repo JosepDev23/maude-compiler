7daf4281d32f3f0e68e3cc40671037340f66d23d
JosepDev23 <josepdev23@gmail.com>
2025-07-31 18:17:28 +0200

    [CREATE] modules auth and users

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common'
 import { SwaggerModule } from '@nestjs/swagger'
 import { MaudeContainerModule } from './docker/maude-container.module'
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [SwaggerModule, MaudeContainerModule],
+  imports: [SwaggerModule, MaudeContainerModule, AuthModule, UsersModule],
 })
 export class AppModule {}
